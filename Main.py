ff9e669222e480b2b39c7aee7c81f334334263d4
Tolgahan Dikmen <tolgahandikmen@hotmail.com>
2019-04-12 16:04:06 +0300

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,5 +1,11 @@
 import numpy as np
 import math
+from nltk.stem import PorterStemmer
+from TurkishStemmer import TurkishStemmer
+
+
+ps = TurkishStemmer()
+
 
 stateDictionary = {}
 tagWordDictionary = {}
@@ -14,6 +20,8 @@ def main():
     non_blank_count = 0
     testSetTotalTagCount = 0
     testSetTotalTrueTaggingCounter = 0
+
+    punctuationList = ['(', ')', '?', ':', ';', ',', '.', '!', '/', '"', "'"]
 
     with open('metu.txt', encoding="utf8") as f:
         for line in f:
@@ -55,6 +63,7 @@ def main():
 
             ###  Tag-Word Dictionary Building Part  ###
             currentWord = word[0].lower()
+            # currentWord = ps.stem(currentWord)
 
             if currentState in tagWordDictionary.keys():
                 if currentWord in tagWordDictionary[currentState]:
@@ -97,6 +106,7 @@ def main():
     ###  TASK II  ###
     stateDictionaryKeyCount = len(stateDictionary.keys())
     tagWordDictionaryKeyCount = len(tagWordDictionary.keys())
+    puncIndexInMatrix = 0
     for index in range(0, int(non_blank_count * 0.3)+2):
         line = file.readline()
         words = line.split()
@@ -113,40 +123,67 @@ def main():
         tempListForStartState.remove("totalValueOfTheState")
         # print(tempListForStartState)
 
+        ## Creating matrix structure ##
         for k in range(0, stateDictionaryKeyCount-1):
             matrix[k+1][0] = tempList[k]
-
+            if 'Punc' == tempList[k]:
+                puncIndexInMatrix = k+1
 
         for k in range(0, len(words)):
             word = words[k].split("/")
+            word[0] = word[0].lower()
+            # word[0] = ps.stem(word[0])
             matrix[0][k+1] = word[0]
             trueTags.append(word[1])
             testSetTotalTagCount += 1
+        ## Creating matrix structure ##
 
-        ## For initial probabilities, building first column of the matrix ##
-        # for k in range(1, stateDictionaryKeyCount):
-        #     if matrix[k][0] in tempListForStartState:
-        #         matrix[k][1] = (1 + stateDictionary["Start"][matrix[k][0]]) / (stateDictionaryKeyCount + stateDictionary["Start"]["totalValueOfTheState"])
-        #     else:
-        #         matrix[k][1] = 1 / (stateDictionaryKeyCount + stateDictionary["Start"]["totalValueOfTheState"])
-        ## For initial probabilities, building first column of the matrix ##
 
         ## For initial probabilities, building first column of the matrix ##
         for k in range(1, stateDictionaryKeyCount):
-            if matrix[k][0] in tempListForStartState:
-                if matrix[0][1] in tagWordDictionary[matrix[k][0]]:
-                    matrix[k][1] = [math.log10((1 + stateDictionary["Start"][matrix[k][0]]) / (vocabularySize + stateDictionary["Start"]["totalValueOfTheState"]) *
-                                               ((1 + tagWordDictionary[matrix[k][0]][matrix[0][1]]) / (tagWordDictionary[matrix[k][0]]["totalValueOfTheState"] + tagWordVocabularySize))), matrix[k][0]]
+            if matrix[0][1] not in punctuationList:
+                if matrix[k][0] in tempListForStartState:
+                    if matrix[0][1] in tagWordDictionary[matrix[k][0]]:
+                        matrix[k][1] = [math.log10((1 + stateDictionary["Start"][matrix[k][0]]) / (vocabularySize + stateDictionary["Start"]["totalValueOfTheState"]) *
+                                                   ((1 + tagWordDictionary[matrix[k][0]][matrix[0][1]]) / (tagWordDictionary[matrix[k][0]]["totalValueOfTheState"] + tagWordVocabularySize))), matrix[k][0]]
+                    else:
+                        matrix[k][1] = [math.log10((1 + stateDictionary["Start"][matrix[k][0]]) / (vocabularySize + stateDictionary["Start"]["totalValueOfTheState"]) *
+                                                   (1 / (tagWordDictionary[matrix[k][0]]["totalValueOfTheState"] + tagWordVocabularySize))), matrix[k][0]]
                 else:
-                    matrix[k][1] = [math.log10((1 + stateDictionary["Start"][matrix[k][0]]) / (vocabularySize + stateDictionary["Start"]["totalValueOfTheState"]) *
-                                               (1 / (tagWordDictionary[matrix[k][0]]["totalValueOfTheState"] + tagWordVocabularySize))), matrix[k][0]]
+                    if matrix[0][1] in tagWordDictionary[matrix[k][0]]:
+                        matrix[k][1] = [math.log10(1 / (vocabularySize + stateDictionary["Start"]["totalValueOfTheState"]) *
+                                                   ((1 + tagWordDictionary[matrix[k][0]][matrix[0][1]]) / (tagWordDictionary[matrix[k][0]]["totalValueOfTheState"] + tagWordVocabularySize))), matrix[k][0]]
+                    else:
+                        matrix[k][1] = [math.log10(1 / (vocabularySize + stateDictionary["Start"]["totalValueOfTheState"]) *
+                                                   (1 / (tagWordDictionary[matrix[k][0]]["totalValueOfTheState"] + tagWordVocabularySize))), matrix[k][0]]
             else:
-                if matrix[0][1] in tagWordDictionary[matrix[k][0]]:
-                    matrix[k][1] = [math.log10(1 / (vocabularySize + stateDictionary["Start"]["totalValueOfTheState"]) *
-                                               ((1 + tagWordDictionary[matrix[k][0]][matrix[0][1]]) / (tagWordDictionary[matrix[k][0]]["totalValueOfTheState"] + tagWordVocabularySize))), matrix[k][0]]
+                if matrix[k][0] in tempListForStartState:
+                    if matrix[0][1] in tagWordDictionary[matrix[k][0]]:
+                        matrix[k][1] = [math.log10((1 + stateDictionary["Start"][matrix[k][0]]) / (
+                                    vocabularySize + stateDictionary["Start"]["totalValueOfTheState"]) *
+                                                   ((1 + tagWordDictionary[matrix[k][0]][matrix[0][1]]) / (
+                                                               tagWordDictionary[matrix[k][0]][
+                                                                   "totalValueOfTheState"] + tagWordVocabularySize))),
+                                        matrix[k][0]]
+                    else:
+                        matrix[k][1] = [math.log10((1 + stateDictionary["Start"][matrix[k][0]]) / (
+                                    vocabularySize + stateDictionary["Start"]["totalValueOfTheState"]) *
+                                                   (1 / (tagWordDictionary[matrix[k][0]][
+                                                             "totalValueOfTheState"] + tagWordVocabularySize))),
+                                        matrix[k][0]]
                 else:
-                    matrix[k][1] = [math.log10(1 / (vocabularySize + stateDictionary["Start"]["totalValueOfTheState"]) *
-                                               (1 / (tagWordDictionary[matrix[k][0]]["totalValueOfTheState"] + tagWordVocabularySize))), matrix[k][0]]
+                    if matrix[0][1] in tagWordDictionary[matrix[k][0]]:
+                        matrix[k][1] = [
+                            math.log10(1 / (vocabularySize + stateDictionary["Start"]["totalValueOfTheState"]) *
+                                       ((1 + tagWordDictionary[matrix[k][0]][matrix[0][1]]) / (
+                                                   tagWordDictionary[matrix[k][0]][
+                                                       "totalValueOfTheState"] + tagWordVocabularySize))), matrix[k][0]]
+                    else:
+                        matrix[k][1] = [
+                            math.log10(1 / (vocabularySize + stateDictionary["Start"]["totalValueOfTheState"]) *
+                                       (1 / (tagWordDictionary[matrix[k][0]][
+                                                 "totalValueOfTheState"] + tagWordVocabularySize))), matrix[k][0]]
+                matrix[puncIndexInMatrix][1] = [math.log10(1), matrix[puncIndexInMatrix][0]]
 
         ## For initial probabilities, building first column of the matrix ##
 
@@ -162,24 +199,45 @@ def main():
                 for t in range(1, stateDictionaryKeyCount):
                     arrangePosition = matrix[t][0]
                     # print(currentPosition, arrangePosition, k, j)
-                    if currentPosition in stateDictionary[arrangePosition]:
-                        if matrix[0][j] in tagWordDictionary[currentPosition]:
-                            total = math.log10(((1 + stateDictionary[arrangePosition][currentPosition]) / (stateDictionary[arrangePosition]["totalValueOfTheState"] + vocabularySize)) \
-                                    * ((1 + tagWordDictionary[currentPosition][matrix[0][j]]) / (tagWordDictionary[currentPosition]["totalValueOfTheState"] + tagWordVocabularySize)))\
-                                    + (matrix[t][j-1][0])
+                    if matrix[0][1] not in punctuationList:
+                        if currentPosition in stateDictionary[arrangePosition]:
+                            if matrix[0][j] in tagWordDictionary[currentPosition]:
+                                total = math.log10(((1 + stateDictionary[arrangePosition][currentPosition]) / (stateDictionary[arrangePosition]["totalValueOfTheState"] + vocabularySize)) \
+                                        * ((1 + tagWordDictionary[currentPosition][matrix[0][j]]) / (tagWordDictionary[currentPosition]["totalValueOfTheState"] + tagWordVocabularySize)))\
+                                        + (matrix[t][j-1][0])
+                            else:
+                                total = math.log10(((1 + stateDictionary[arrangePosition][currentPosition]) / (stateDictionary[arrangePosition]["totalValueOfTheState"] + vocabularySize)) \
+                                        * (1 / (tagWordDictionary[currentPosition]["totalValueOfTheState"] + tagWordVocabularySize))) \
+                                        + (matrix[t][j - 1][0])
                         else:
-                            total = math.log10(((1 + stateDictionary[arrangePosition][currentPosition]) / (stateDictionary[arrangePosition]["totalValueOfTheState"] + vocabularySize)) \
-                                    * (1 / (tagWordDictionary[currentPosition]["totalValueOfTheState"] + tagWordVocabularySize))) \
-                                    + (matrix[t][j - 1][0])
+                            if matrix[0][j] in tagWordDictionary[currentPosition]:
+                                total = math.log10((1 / (stateDictionary[arrangePosition]["totalValueOfTheState"] + vocabularySize)) \
+                                        * ((1 + tagWordDictionary[currentPosition][matrix[0][j]]) / (tagWordDictionary[currentPosition]["totalValueOfTheState"] + tagWordVocabularySize)))\
+                                        + (matrix[t][j-1][0])
+                            else:
+                                total = math.log10((1 / (stateDictionary[arrangePosition]["totalValueOfTheState"] + vocabularySize)) \
+                                        * (1 / (tagWordDictionary[currentPosition]["totalValueOfTheState"] + tagWordVocabularySize))) \
+                                        + (matrix[t][j - 1][0])
                     else:
-                        if matrix[0][j] in tagWordDictionary[currentPosition]:
-                            total = math.log10((1 / (stateDictionary[arrangePosition]["totalValueOfTheState"] + vocabularySize)) \
-                                    * ((1 + tagWordDictionary[currentPosition][matrix[0][j]]) / (tagWordDictionary[currentPosition]["totalValueOfTheState"] + tagWordVocabularySize)))\
-                                    + (matrix[t][j-1][0])
+                        if currentPosition in stateDictionary[arrangePosition]:
+                            if matrix[0][j] in tagWordDictionary[currentPosition]:
+                                total = math.log10(((1 + stateDictionary[arrangePosition][currentPosition]) / (stateDictionary[arrangePosition]["totalValueOfTheState"] + vocabularySize)) \
+                                        * ((1 + tagWordDictionary[currentPosition][matrix[0][j]]) / (tagWordDictionary[currentPosition]["totalValueOfTheState"] + tagWordVocabularySize)))\
+                                        + (matrix[t][j-1][0])
+                            else:
+                                total = math.log10(((1 + stateDictionary[arrangePosition][currentPosition]) / (stateDictionary[arrangePosition]["totalValueOfTheState"] + vocabularySize)) \
+                                        * (1 / (tagWordDictionary[currentPosition]["totalValueOfTheState"] + tagWordVocabularySize))) \
+                                        + (matrix[t][j - 1][0])
                         else:
-                            total = math.log10((1 / (stateDictionary[arrangePosition]["totalValueOfTheState"] + vocabularySize)) \
-                                    * (1 / (tagWordDictionary[currentPosition]["totalValueOfTheState"] + tagWordVocabularySize))) \
-                                    + (matrix[t][j - 1][0])
+                            if matrix[0][j] in tagWordDictionary[currentPosition]:
+                                total = math.log10((1 / (stateDictionary[arrangePosition]["totalValueOfTheState"] + vocabularySize)) \
+                                        * ((1 + tagWordDictionary[currentPosition][matrix[0][j]]) / (tagWordDictionary[currentPosition]["totalValueOfTheState"] + tagWordVocabularySize)))\
+                                        + (matrix[t][j-1][0])
+                            else:
+                                total = math.log10((1 / (stateDictionary[arrangePosition]["totalValueOfTheState"] + vocabularySize)) \
+                                        * (1 / (tagWordDictionary[currentPosition]["totalValueOfTheState"] + tagWordVocabularySize))) \
+                                        + (matrix[t][j - 1][0])
+
                     findMaxArray.append(total)
                     pathArray.append(matrix[t][0])
                 maxValue = max(findMaxArray)
@@ -200,6 +258,9 @@ def main():
         for i in range(len(words)-1, 1, -1):
             index = tempList.index(path[0]) + 1
             path.insert(0, matrix[index][i][1])
+
+        # for i in range(1,len(words)):
+        #     print(matrix[0][i], end = ' ')
         # print(path)
 
         # burayı aç!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
@@ -216,6 +277,11 @@ def main():
     ###  TASK III  ###
     print(testSetTotalTrueTaggingCounter/testSetTotalTagCount, testSetTotalTrueTaggingCounter, testSetTotalTagCount)
     ###  TASK III  ###
+
+
+    # for i in stateDictionary:
+    #     print(i, stateDictionary[i]["totalValueOfTheState"])
+
 
     ###  State Model Dictionary Possibilities (Transition Probability) Printing  ###
     # for key in statePossibilitiesDictionary:
